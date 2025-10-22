d9479ae367b3d2a0f441487fe3ac8d3864338a7d
Aniq Nazhan BIN MAZLAN <aniqnazhanmazlan05@gmail.com>
2025-10-22 14:16:40 +0800

    commit

---
+++ test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Patients 
+    ( PatientID INT PRIMARY KEY AUTO_INCREMENT, 
+    FirstName VARCHAR(50) NOT NULL, 
+    LastName VARCHAR(50) NOT NULL, 
+    DateOfBirth DATE NOT NULL 
+    );
