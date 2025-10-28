556437e0f130e44606bdbf633c0905fff55c2f5d
Suerfine <nathanlgx@gmail.com>
2025-10-28 10:19:34 +0800

    feat(sql): Mysqldump of the current database structure

---
+++ cos10082_database.sql
@@ -0,0 +1,330 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2025 at 05:40 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cos10082_med_manage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `AppointmentID` int(11) NOT NULL,
+  `PatientUserID` int(11) NOT NULL,
+  `DoctorUserID` int(11) NOT NULL,
+  `AppointmentOn` date NOT NULL,
+  `Details` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctordetails`
+--
+
+CREATE TABLE `doctordetails` (
+  `DoctorDetailsID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Specialization` varchar(100) NOT NULL,
+  `MedicalLicenseNumber` varchar(50) NOT NULL,
+  `YearsOfExperience` int(11) NOT NULL,
+  `MedicalSchool` varchar(50) NOT NULL,
+  `Certificates` varchar(30) NOT NULL,
+  `LanguagesSpoken` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicaldetails`
+--
+
+CREATE TABLE `medicaldetails` (
+  `PatientDetailsID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `ABOBloodType` enum('A','B','AB','O') NOT NULL,
+  `RhBloodType` enum('+','-') NOT NULL,
+  `EmergencyContact` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine`
+--
+
+CREATE TABLE `medicine` (
+  `MedicineId` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Brand` varchar(50) NOT NULL,
+  `Dosage` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `PrescriptionID` int(11) NOT NULL,
+  `PatientUserID` int(11) NOT NULL,
+  `DoctorUserID` int(11) NOT NULL,
+  `Remark` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminder`
+--
+
+CREATE TABLE `reminder` (
+  `ReminderID` int(11) NOT NULL,
+  `ScheduleID` int(11) NOT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `IsMedicationTaken` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `ScheduleID` int(11) NOT NULL,
+  `PrescriptionID` int(11) NOT NULL,
+  `MedicineID` int(11) NOT NULL,
+  `StartDate` timestamp NULL DEFAULT NULL,
+  `EndDate` timestamp NULL DEFAULT NULL,
+  `IntervalMillisecond` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sideeffect`
+--
+
+CREATE TABLE `sideeffect` (
+  `SideEffectID` int(11) NOT NULL,
+  `PrescriptionID` int(11) NOT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Description` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserId` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `PhoneNumber` varchar(20) NOT NULL,
+  `Password` char(255) NOT NULL,
+  `UserType` enum('superadmin','admin','doctor','patient') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`AppointmentID`),
+  ADD KEY `DoctorUserID` (`DoctorUserID`),
+  ADD KEY `PatientUserID` (`PatientUserID`);
+
+--
+-- Indexes for table `doctordetails`
+--
+ALTER TABLE `doctordetails`
+  ADD PRIMARY KEY (`DoctorDetailsID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `medicaldetails`
+--
+ALTER TABLE `medicaldetails`
+  ADD PRIMARY KEY (`PatientDetailsID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `medicine`
+--
+ALTER TABLE `medicine`
+  ADD PRIMARY KEY (`MedicineId`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`PrescriptionID`),
+  ADD KEY `DoctorUserID` (`DoctorUserID`),
+  ADD KEY `PatientUserID` (`PatientUserID`);
+
+--
+-- Indexes for table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD PRIMARY KEY (`ReminderID`),
+  ADD KEY `ScheduleID` (`ScheduleID`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`ScheduleID`),
+  ADD KEY `PrescriptionID` (`PrescriptionID`),
+  ADD KEY `MedicineID` (`MedicineID`);
+
+--
+-- Indexes for table `sideeffect`
+--
+ALTER TABLE `sideeffect`
+  ADD PRIMARY KEY (`SideEffectID`),
+  ADD KEY `PrescriptionID` (`PrescriptionID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `AppointmentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctordetails`
+--
+ALTER TABLE `doctordetails`
+  MODIFY `DoctorDetailsID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `medicaldetails`
+--
+ALTER TABLE `medicaldetails`
+  MODIFY `PatientDetailsID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `medicine`
+--
+ALTER TABLE `medicine`
+  MODIFY `MedicineId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+  MODIFY `PrescriptionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reminder`
+--
+ALTER TABLE `reminder`
+  MODIFY `ReminderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `ScheduleID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sideeffect`
+--
+ALTER TABLE `sideeffect`
+  MODIFY `SideEffectID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`DoctorUserID`) REFERENCES `user` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`PatientUserID`) REFERENCES `user` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `doctordetails`
+--
+ALTER TABLE `doctordetails`
+  ADD CONSTRAINT `doctordetails_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `medicaldetails`
+--
+ALTER TABLE `medicaldetails`
+  ADD CONSTRAINT `medicaldetails_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`DoctorUserID`) REFERENCES `user` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`PatientUserID`) REFERENCES `user` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD CONSTRAINT `reminder_ibfk_1` FOREIGN KEY (`ScheduleID`) REFERENCES `schedule` (`ScheduleID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`PrescriptionID`) REFERENCES `prescription` (`PrescriptionID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`MedicineID`) REFERENCES `medicine` (`MedicineId`);
+
+--
+-- Constraints for table `sideeffect`
+--
+ALTER TABLE `sideeffect`
+  ADD CONSTRAINT `sideeffect_ibfk_1` FOREIGN KEY (`PrescriptionID`) REFERENCES `prescription` (`PrescriptionID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
