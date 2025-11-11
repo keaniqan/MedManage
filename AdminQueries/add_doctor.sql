f6b59377c407e36c45bb466b8cce4c6725a04bd4
Aniq Nazhan BIN MAZLAN <aniqnazhanmazlan05@gmail.com>
2025-11-12 01:14:51 +0800

    aa

--- AdminQueries/add_doctor.sql
+++ AdminQueries/add_doctor.sql
@@ -1,19 +1,20 @@
 DELIMITER //
-
+DROP PROCEDURE IF EXISTS AddDoctor;
 CREATE PROCEDURE AddDoctor(
     IN p_UserID INT,
     IN p_Username VARCHAR(100),
     IN p_Email VARCHAR(100),
     IN p_FirstName VARCHAR(100),
     IN p_LastName VARCHAR(100),
-    IN p_Phone VARCHAR(20),
+    IN p_Phone VARCHAR(259),
     IN p_Password VARCHAR(255),
     IN p_Identification VARCHAR(20),
-    IN p_Gender ENUM('M','F'),
+    IN p_Gender CHAR(1),
     IN p_InstituteID SMALLINT
 )
 BEGIN
-    INSERT INTO users (
+    -- Insert into app table
+    INSERT INTO user (
         UserID, Username, Email, UserType,
         FirstName, LastName, Phone, PasswordHash,
         Identification, Gender, InstituteID
@@ -24,15 +25,18 @@ BEGIN
         p_Identification, p_Gender, p_InstituteID
     );
 
-    SET @sql1 = CONCAT('CREATE USER \'', p_Username, '\'@\'localhost\' IDENTIFIED BY \'', p_Password, '\';');
-    PREPARE stmt1 FROM @sql1; EXECUTE stmt1; DEALLOCATE PREPARE stmt1;
+    -- Build dynamic SQL for CREATE USER and GRANT
+    SET @create_sql = CONCAT('CREATE USER IF NOT EXISTS \'', REPLACE(p_Username, '''', '\\'''), '\'@\'localhost\' IDENTIFIED BY \'', REPLACE(p_Password, '''', '\\'''), '\';');
+    PREPARE stmt1 FROM @create_sql;
+    EXECUTE stmt1;
+    DEALLOCATE PREPARE stmt1;
 
-    SET @sql2 = CONCAT('GRANT SELECT, INSERT, UPDATE ON medmanagedb.users TO \'', p_Username, '\'@\'localhost\';');
-    PREPARE stmt2 FROM @sql2; EXECUTE stmt2; DEALLOCATE PREPARE stmt2;
+    SET @grant_sql = CONCAT('GRANT SELECT, INSERT, UPDATE ON medmanagedb.user TO \'', REPLACE(p_Username, '''', '\\'''), '\'@\'localhost\';');
+    PREPARE stmt2 FROM @grant_sql;
+    EXECUTE stmt2;
+    DEALLOCATE PREPARE stmt2;
 
     FLUSH PRIVILEGES;
 END //
 
 DELIMITER ;
-
-CALL AddDoctor(1, 'doc1', 'dr.jones@example.com', 'Sarah', 'Jones', '0123456789', 'doc1', 'IC1234567', 'F', 1);
--- cos10082_database.sql
+++ cos10082_database.sql
@@ -1,3 +1,4 @@
+-- Active: 1761104084214@@127.0.0.1@3307@medmanagedb
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
