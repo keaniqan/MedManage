dfb3f29b84aea672cbb471435caf224728d42843
Aniq Nazhan BIN MAZLAN <aniqnazhanmazlan05@gmail.com>
2025-11-12 00:33:56 +0800

    WWW

--- AdminQueries/add_doctor.sql
+++ AdminQueries/add_doctor.sql
@@ -1,31 +1,38 @@
-INSERT INTO users (
-  UserID,
-  Username,
-  Email,
-  UserType,
-  FirstName,
-  LastName,
-  Phone,
-  PasswordHash,
-  Identification,
-  Gender,
-  InstituteID
+DELIMITER //
+
+CREATE PROCEDURE AddDoctor(
+    IN p_UserID INT,
+    IN p_Username VARCHAR(100),
+    IN p_Email VARCHAR(100),
+    IN p_FirstName VARCHAR(100),
+    IN p_LastName VARCHAR(100),
+    IN p_Phone VARCHAR(20),
+    IN p_Password VARCHAR(255),
+    IN p_Identification VARCHAR(20),
+    IN p_Gender ENUM('M','F'),
+    IN p_InstituteID SMALLINT
 )
-VALUES (
-  1,
-  'drjones',
-  'dr.jones@example.com',
-  'doctor',
-  'Sarah',
-  'Jones',
-  '0123456789',
-  SHA2('securePassword123', 256),  -- Hash the password securely
-  'IC1234567',
-  'F',
-  1
-);
+BEGIN
+    INSERT INTO users (
+        UserID, Username, Email, UserType,
+        FirstName, LastName, Phone, PasswordHash,
+        Identification, Gender, InstituteID
+    ) VALUES (
+        p_UserID, p_Username, p_Email, 'doctor',
+        p_FirstName, p_LastName, p_Phone,
+        SHA2(p_Password, 256),
+        p_Identification, p_Gender, p_InstituteID
+    );
 
-CREATE USER 'doc1'@'localhost' IDENTIFIED BY 'doc1';
-GRANT SELECT, INSERT, UPDATE ON medmanagedb.users TO 'doc1'@'localhost';
-FLUSH PRIVILEGES;
+    SET @sql1 = CONCAT('CREATE USER \'', p_Username, '\'@\'localhost\' IDENTIFIED BY \'', p_Password, '\';');
+    PREPARE stmt1 FROM @sql1; EXECUTE stmt1; DEALLOCATE PREPARE stmt1;
 
+    SET @sql2 = CONCAT('GRANT SELECT, INSERT, UPDATE ON medmanagedb.users TO \'', p_Username, '\'@\'localhost\';');
+    PREPARE stmt2 FROM @sql2; EXECUTE stmt2; DEALLOCATE PREPARE stmt2;
+
+    FLUSH PRIVILEGES;
+END //
+
+DELIMITER ;
+
+CALL AddDoctor(1, 'doc1', 'dr.jones@example.com', 'Sarah', 'Jones', '0123456789', 'doc1', 'IC1234567', 'F', 1);
